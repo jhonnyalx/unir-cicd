6de820774130d3cb821533e3a2f856525f0f0245
jhonnyalx <jhonnyzapat@gmail.com>
2025-06-22 04:19:26 -0500

    test

--- test/e2e/cypress/integration/calc.spec.js
+++ test/e2e/cypress/integration/calc.spec.js
@@ -17,10 +17,11 @@ context('Calc', () => {
   })
 
   it('can click add', () => {
+    cy.intercept('GET', '**/calc/add/2/3').as('addOperation')
     cy.get('#in-op1').clear().type('2')
     cy.get('#in-op2').clear().type('3')
     cy.get('#button-add').click()
-    cy.wait(1000) // Esperar a que la API responda
+    cy.wait('@addOperation')
     cy.get('#result-area').should('have.text', "Result: 5")
     cy.screenshot()
   })
@@ -34,33 +35,41 @@ context('Calc', () => {
   })
 
   it('can click substract (using fixture)', () => {
-    cy.intercept('GET', '**/calc/substract/4/-4', '8').as('getResult')
+    cy.intercept('GET', '**/calc/substract/4/-4', {
+      statusCode: 200,
+      body: "8"
+    }).as('getResult')
 
     cy.get('#in-op1').clear().type('4')
     cy.get('#in-op2').clear().type('-4')
     cy.get('#button-substract').click()
 
     cy.wait('@getResult')
-
     cy.get('#result-area').should('have.text', "Result: 8")
     cy.screenshot()
   })
 
   it('increases the history log', () => {
+    cy.intercept('GET', '**/calc/add/1/1').as('add1')
+    cy.intercept('GET', '**/calc/add/2/2').as('add2')
+    cy.intercept('GET', '**/calc/add/3/3').as('add3')
+
     cy.get('#in-op1').clear().type('1')
     cy.get('#in-op2').clear().type('1')
     cy.get('#button-add').click()
-    cy.wait(1000) // Esperar a que la primera operación termine
+    cy.wait('@add1')
+
     cy.get('#in-op1').clear().type('2')
     cy.get('#in-op2').clear().type('2')
     cy.get('#button-add').click()
-    cy.wait(1000) // Esperar a que la segunda operación termine
+    cy.wait('@add2')
+
     cy.get('#in-op1').clear().type('3')
     cy.get('#in-op2').clear().type('3')
     cy.get('#button-add').click()
-    cy.wait(1000) // Esperar a que la tercera operación termine
-    cy.get('#history-log').children().its('length')
-    .should('eq', 3)
+    cy.wait('@add3')
+
+    cy.get('#history-log').children().should('have.length', 3)
     cy.screenshot()
   })
 
